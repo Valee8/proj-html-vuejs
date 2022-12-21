083435030463bf6194a8e5808bd1d3a91e6d91c8
Vale <oirelav95@gmail.com>
2022-12-21 17:21:43 +0100

    aggiungo contenuto sezione our-products

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,12 +7,94 @@ export default {
 
 <template>
 
-    <h1>
-        Prova
-    </h1>
+    <section id="our-products">
+
+        <div class="container">
+            <div class="text-section">
+                <div class="mini-title">
+                    Our Products
+                </div>
+
+                <h2>
+                    All our delectable pastries are backed fresh in our Kitchen very morning, and are made with
+                    all-natural,
+                    all
+                    organic ingredients
+                </h2>
+
+                <a href="#" class="btn violet">
+                    Start Shopping
+                </a>
+            </div>
+
+            <div class="img-section">
+                <img src="/img/choco-chip-cookies-400x510.jpg" alt="Choco Chip Cookies">
+                <img src="/img/strawberry-jam-cookies-400x510.jpg" alt="Straberry Jam Cookies">
+
+                <a href="#" class="left-arrow">
+                    <font-awesome-icon icon="fa-solid fa-angle-left" />
+                </a>
+
+                <a href="#" class="right-arrow">
+                    <font-awesome-icon icon="fa-solid fa-angle-right" />
+                </a>
+            </div>
+        </div>
+
+    </section>
 
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
+
+section {
+    padding: 100px 0;
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+
+        .text-section {
+            width: 30%;
+
+            & *:not(.btn) {
+                padding-bottom: 20px;
+            }
+        }
+
+        .img-section {
+            position: relative;
+
+            img {
+                margin: 0 8px;
+                vertical-align: middle;
+            }
+
+            .left-arrow {
+                left: 8px;
+            }
+
+            .right-arrow {
+                right: 8px;
+            }
+
+            .left-arrow,
+            .right-arrow {
+                position: absolute;
+                top: 50%;
+                transform: translateY(-50%);
+                background-color: #a691b2;
+                padding: 22px 11px;
+                color: $white-color;
+                font-size: 0.9rem;
+
+                a {
+                    color: #e9dedc;
+                }
+            }
+        }
+    }
+}
 </style>
