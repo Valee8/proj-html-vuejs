03d725e3943ec503deac605a372dd958f9e55c6b
Vale <oirelav95@gmail.com>
2022-12-21 22:35:33 +0100

    importo AppJumbotron in AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,13 @@
 <script>
 
 import AppNav from '../components/AppNav.vue'
+import AppJumbotron from '../components/AppJumbotron.vue'
 
 export default {
     name: "AppHeader",
     components: {
-        AppNav
+        AppNav,
+        AppJumbotron
     }
 }
 </script>
@@ -16,52 +18,7 @@ export default {
 
         <AppNav />
 
-        <div class="jumbotron">
-
-            <div class="text-content">
-                <h5>
-                    Fresh &amp; Tasty Bakery Everyday
-                </h5>
-
-                <h1>
-                    The Perfect Fresh Bread
-                </h1>
-
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita veniam illum corrupti commodi iste
-                    asperiores
-                </p>
-
-                <a href="#" class="btn white">
-                    Explore our products
-                </a>
-
-            </div>
-
-            <ul class="header-icons">
-
-                <li>
-                    <a href="#">
-                        Icona
-                    </a>
-                </li>
-
-                <li>
-                    <a href="#">
-                        <div class="number">
-                            <sup>
-                                &dollar;
-                            </sup>
-                            39
-                        </div>
-                        <div class="on-sale">
-                            On sale
-                        </div>
-                    </a>
-                </li>
-
-            </ul>
-        </div>
+        <AppJumbotron />
 
     </header>
 
@@ -77,52 +34,5 @@ header {
     background-position: 0 -60px;
     background-size: cover;
 
-    .jumbotron {
-        width: 100%;
-        min-height: 530px;
-        padding: 120px 0 0 80px;
-        position: relative;
-
-        .header-icons {
-            list-style-type: none;
-            position: absolute;
-            right: 13px;
-            top: 40px;
-            text-align: center;
-
-            li {
-                background-color: $white-color;
-                margin: 5px 0;
-                border-radius: 3px;
-                padding: 7px 4px;
-                font-weight: bold;
-                box-shadow: 0 0 20px rgba(0, 0, 0, .2);
-
-                .number {
-                    color: #65bc7b;
-                    font-size: 1.2rem;
-
-                    sup {
-                        font-size: 0.8rem;
-                        letter-spacing: -2px;
-                    }
-                }
-
-                .on-sale {
-                    font-size: 0.6rem;
-                }
-            }
-        }
-
-        .text-content {
-            width: 350px;
-            color: $text-color;
-
-            h1 {
-                color: $dark-violet-color;
-            }
-
-        }
-    }
 }
 </style>
