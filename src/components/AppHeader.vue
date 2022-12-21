59e55b1ddd56dec6eb651cfada9ecd1c167de000
Vale <oirelav95@gmail.com>
2022-12-21 16:36:30 +0100

    aggiungo icone a destra dell'header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -61,11 +61,12 @@ export default {
 
     <header>
 
-        <a href="/">
-            <img src="/img/avada-bakery-logo.png" alt="logo bakery" class="logo">
-        </a>
-
         <nav>
+
+            <a href="/">
+                <img src="/img/avada-bakery-logo.png" alt="logo bakery" class="logo">
+            </a>
+
             <ul class="links">
                 <li v-for="(link, index) in navbarLinks" :key="index" :class="{ active: link.current }">
                     <a :href="link.url">
@@ -100,6 +101,30 @@ export default {
                 </a>
 
             </div>
+
+            <ul class="header-icons">
+
+                <li>
+                    <a href="#">
+                        Icona
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <div class="number">
+                            <sup>
+                                &dollar;
+                            </sup>
+                            39
+                        </div>
+                        <div class="on-sale">
+                            On sale
+                        </div>
+                    </a>
+                </li>
+
+            </ul>
         </div>
 
     </header>
@@ -112,41 +137,76 @@ export default {
 
 
 header {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 20px;
     background-image: url('/img/hero-header-bakery.jpg');
     background-position: 0 -60px;
     background-size: cover;
 
-    .logo {
-        width: 150px;
-    }
-
-    .links {
+    nav {
         display: flex;
-        list-style-type: none;
-        gap: 20px;
-        font-size: 0.6rem;
-        text-transform: uppercase;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 30px;
 
-        a {
-            color: $dark-violet-color;
+        .logo {
+            width: 150px;
+        }
+
+        .links {
+            display: flex;
+            list-style-type: none;
+            gap: 20px;
+            font-size: 0.6rem;
+            text-transform: uppercase;
+
+            a {
+                color: $dark-violet-color;
+            }
         }
     }
 
     .jumbotron {
         width: 100%;
-        min-height: 530px;
-        padding: 80px 60px 0 60px;
+        min-height: 540px;
+        padding: 120px 0 0 80px;
+        position: relative;
+
+        .header-icons {
+            list-style-type: none;
+            position: absolute;
+            right: 13px;
+            top: 40px;
+            text-align: center;
+
+            li {
+                background-color: $white-color;
+                margin: 5px 0;
+                border-radius: 3px;
+                padding: 7px 4px;
+                font-weight: bold;
+                box-shadow: 0 0 20px rgba(0, 0, 0, .2);
+
+                .number {
+                    color: #65bc7b;
+                    font-size: 1.2rem;
+
+                    sup {
+                        font-size: 0.8rem;
+                        letter-spacing: -2px;
+                    }
+                }
+
+                .on-sale {
+                    font-size: 0.6rem;
+                }
+            }
+        }
 
         .header-content {
             width: 350px;
             color: $text-color;
 
-            &>*:not(.btn) {
+            & *:not(.btn) {
                 padding-bottom: 20px;
             }
 
