6e49bac5e799cd1fc516997b616ede2387cc489d
Vale <oirelav95@gmail.com>
2022-12-21 17:57:26 +0100

    importo componente OurProducts

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,47 +1,19 @@
 <script>
 
+import OurProducts from '../components/OurProducts.vue'
+
+
 export default {
     name: "AppMain",
+    components: {
+        OurProducts,
+    }
 }
 </script>
 
 <template>
 
-    <section id="our-products">
-
-        <div class="container flex">
-            <div class="text-content">
-                <div class="mini-title">
-                    Our Products
-                </div>
-
-                <h2>
-                    All our delectable pastries are backed fresh in our Kitchen very morning, and are made with
-                    all-natural,
-                    all
-                    organic ingredients
-                </h2>
-
-                <a href="#" class="btn violet">
-                    Start Shopping
-                </a>
-            </div>
-
-            <div class="img-content">
-                <img src="/img/choco-chip-cookies-400x510.jpg" alt="Choco Chip Cookies">
-                <img src="/img/strawberry-jam-cookies-400x510.jpg" alt="Straberry Jam Cookies">
-
-                <a href="#" class="left-arrow">
-                    <font-awesome-icon icon="fa-solid fa-angle-left" />
-                </a>
-
-                <a href="#" class="right-arrow">
-                    <font-awesome-icon icon="fa-solid fa-angle-right" />
-                </a>
-            </div>
-        </div>
-
-    </section>
+    <OurProducts />
 
 </template>
 
@@ -51,50 +23,6 @@ export default {
 
 section {
     padding: 100px 0;
-
-    .container {
-
-        &.flex {
-            display: flex;
-            justify-content: space-between;
-        }
-
-        .text-content {
-            width: 30%;
-        }
-
-        .img-content {
-            position: relative;
-
-            img {
-                margin: 0 8px;
-                vertical-align: middle;
-            }
-
-            .left-arrow {
-                left: 8px;
-            }
-
-            .right-arrow {
-                right: 8px;
-            }
-
-            .left-arrow,
-            .right-arrow {
-                position: absolute;
-                top: 50%;
-                transform: translateY(-50%);
-                background-color: #a691b2;
-                padding: 22px 11px;
-                color: $white-color;
-                font-size: 0.9rem;
-
-                a {
-                    color: #e9dedc;
-                }
-            }
-        }
-    }
 }
 
 #corporate-weddings {
