bb340bd6fa4ef1c17f9f3a342ff836a6776e41ef
Vale <oirelav95@gmail.com>
2022-12-21 12:13:01 +0100

    Creo componente AppFooter

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+    name: "AppFooter",
+}
+</script>
+
+<template>
+
+    <footer>
+        <h1>
+            Prova
+        </h1>
+    </footer>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+
+header {
+    background-color: red;
+}
+</style>
