a3e57fc7b1694b94702c7266009f9731b9c17e4b
Vale <oirelav95@gmail.com>
2022-12-21 20:43:03 +0100

    importo Locations in Main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,14 +3,15 @@
 import OurProducts from '../components/OurProducts.vue'
 import CorporateWeddings from '../components/CorporateWeddings.vue'
 import ListProducts from '../components/ListProducts.vue'
-
+import Locations from '../components/Locations.vue'
 
 export default {
     name: "AppMain",
     components: {
         OurProducts,
         CorporateWeddings,
-        ListProducts
+        ListProducts,
+        Locations,
     }
 }
 </script>
@@ -22,6 +23,8 @@ export default {
     <CorporateWeddings />
 
     <ListProducts />
+
+    <Locations />
 
 </template>
 
