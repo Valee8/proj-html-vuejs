9910908207523a2f732ab460bdf0c1a1ab56dfb6
Vale <oirelav95@gmail.com>
2022-12-21 17:21:05 +0100

    aggiungo nuove icone in main.js

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 /* add icons to the library */
-library.add(faCartShopping)
+library.add(faCartShopping, faAngleLeft, faAngleRight)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
