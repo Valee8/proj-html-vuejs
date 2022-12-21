a9c61263804db2115d944ff4c406eab97703fb4a
Vale <oirelav95@gmail.com>
2022-12-21 13:04:57 +0100

    Aggiungo navbar nell'header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,23 +2,32 @@
 
 export default {
     name: "AppHeader",
+    props: ["links"],
+    data() {
+        return {
+
+        }
+    }
 }
 </script>
 
 <template>
 
     <header>
-        <h1>
-            Prova
-        </h1>
+
+        <nav>
+            <ul>
+                <li v-for="(link, index) in links" :key="index">
+                    <a :href="link.url">
+                        {{ link.text }}</a>
+                </li>
+            </ul>
+        </nav>
+
     </header>
 
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
-
-header {
-    background-color: red;
-}
 </style>
