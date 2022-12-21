76325d613e5c3076b8a5b271e73ffe27ab998716
Vale <oirelav95@gmail.com>
2022-12-21 23:28:29 +0100

    aggiungo contenuto AppFooter

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,20 +1,59 @@
 <script>
 
+import { store } from '../store.js';
+
 export default {
     name: "AppFooter",
+    data() {
+        return {
+            store,
+        }
+    }
 }
 </script>
 
 <template>
 
     <footer>
-        <h1>
-            Prova
-        </h1>
+        <div class="top-footer">
+
+            <img src="/img/avada-bakery-logo.png" alt="logo bakery" class="logo">
+
+            <ul class="links">
+                <li v-for="(link, index) in store.links" :key="index" :class="{ active: link.current }">
+                    <a :href="link.url">
+                        {{ link.text }}</a>
+                </li>
+                <li>
+                    <a href="#">
+                        <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+
+        <div class="bottom-footer">
+
+        </div>
+
+
     </footer>
 
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
+
+.logo {
+    width: 120px;
+}
+
+.links {
+    li {
+        &:first-child {
+            display: none;
+        }
+    }
+}
 </style>
