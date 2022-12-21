59dc38848b2ed56d5739433681a19c612de416b4
Vale <oirelav95@gmail.com>
2022-12-21 18:01:25 +0100

    aggiungo componente CorporateWeddings

---
+++ src/components/CorporateWeddings.vue
@@ -0,0 +1,47 @@
+<script>
+
+export default {
+    name: "CorporateWeddings",
+}
+</script>
+
+<template>
+
+    <section id="corporate-weddings">
+
+        <div class="container">
+            <div class="mini-title">
+                Corporate &amp; Weddings
+            </div>
+
+            <h2>
+                Baking Special Moments
+            </h2>
+
+            <div class="img-content">
+                <img src="/img/corporate-bg.jpg" alt="Corporate">
+                <img src="/img/wedding-bg.jpg" alt="Wedding">
+            </div>
+        </div>
+
+    </section>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
+
+section {
+    text-align: center;
+
+    .container {
+        display: flex;
+        justify-content: space-between;
+
+        img {
+            width: 50%;
+        }
+    }
+}
+</style>
