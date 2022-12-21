1434967b376c47f70fd84c7a7e59c3bcccef55d5
Vale <oirelav95@gmail.com>
2022-12-21 12:13:58 +0100

    importo AppMain in App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
 
 export default {
   name: "App",
   components: {
-    AppHeader
+    AppHeader,
+    AppMain
   }
 }
 </script>
@@ -12,6 +14,12 @@ export default {
 <template>
 
   <AppHeader />
+
+  <main>
+
+    <AppMain />
+
+  </main>
 
 </template>
 
