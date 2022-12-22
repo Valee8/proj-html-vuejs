6fd128d18b111ae542e504a8b77a7be450eab31c
Vale <oirelav95@gmail.com>
2022-12-22 01:04:18 +0100

    importo nuove icone fontawesome

--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-import { faCartShopping, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faAngleLeft, faAngleRight, faFolder } from '@fortawesome/free-solid-svg-icons';
 
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram, faTwitter, faFacebookF, faPinterestP } from '@fortawesome/free-brands-svg-icons';
 
 /* add icons to the library */
-library.add(faCartShopping, faAngleLeft, faAngleRight, faInstagram)
+library.add(faCartShopping, faAngleLeft, faAngleRight, faInstagram, faFolder, faTwitter, faFacebookF, faPinterestP)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
