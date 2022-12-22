f817618942ea0f215f3a0fc67d72a0eac38b0335
Vale <oirelav95@gmail.com>
2022-12-22 03:04:08 +0100

    modifico contenuto ProcessProducts

--- src/components/ProcessProducts.vue
+++ src/components/ProcessProducts.vue
@@ -2,32 +2,6 @@
 
 export default {
     name: "Ingredients",
-    data() {
-        return {
-            cardsIngredient: [
-                {
-                    text1: 'Tell us what you have in mind',
-                    text2: 'Manual Ingredients selection',
-                    text3: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi incidunt iure sed, facilis, ipsum necessitatibus blanditiis, quibusdam architecto aperiam assumenda doloribus porro velit fugit reprehenderit molestias voluptate a veritatis. Fugiat?'
-                },
-                {
-                    text1: 'We Start Baking',
-                    text2: 'Sweet & Delicious',
-                    text3: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi incidunt iure sed, facilis, ipsum necessitatibus blanditiis, quibusdam architecto aperiam assumenda doloribus porro velit fugit reprehenderit molestias voluptate a veritatis. Fugiat?'
-                },
-                {
-                    text1: 'Dont just take our word for it',
-                    text2: 'Finally found an alternative to the mass produced products. Something that incorporates real organic ingredients, nutrient dense wellness while promoting sustainability and activity',
-                    text3: 'Rachel Cooper, Founder'
-                },
-                {
-                    text1: 'Delivered to your home',
-                    text2: 'Baked to order & Gift Wrapped',
-                    text3: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi incidunt iure sed, facilis, ipsum necessitatibus blanditiis, quibusdam architecto aperiam assumenda doloribus porro velit fugit reprehenderit molestias voluptate a veritatis. Fugiat?'
-                }
-            ]
-        }
-    }
 }
 </script>
 
@@ -37,39 +11,101 @@ export default {
 
         <div class="container">
 
-            <div v-for="(infoIngredients, index) in cardsIngredient" :key="index" :class="'card n-' + index">
+            <div class="card one">
 
-                <div class="number" v-if="index != 3">
-                    0{{ index + 1 }}
+                <div class="brightness">
+                    <div class="number">
+                        01
+                    </div>
+
+                    <h1>
+                        Tell us what you have in mind
+                    </h1>
+
+                    <div class="text-card">
+                        <h5>
+                            Manual Ingredients selection
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi incidunt iure sed, facilis,
+                            ipsum
+                            necessitatibus blanditiis, quibusdam architecto aperiam assumenda doloribus porro velit
+                            fugit
+                            reprehenderit molestias voluptate a veritatis. Fugiat?
+                        </p>
+                    </div>
                 </div>
+            </div>
 
-                <div class="number" v-else>
-                    0{{ index }}
+            <div class="card two">
+
+                <div class="brightness">
+                    <div class="number">
+                        02
+                    </div>
+
+                    <h1>
+                        We Start Baking
+                    </h1>
+
+                    <div class="text-card">
+                        <h5>
+                            Sweet & Delicious
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi incidunt iure sed, facilis,
+                            ipsum
+                            necessitatibus blanditiis, quibusdam architecto aperiam assumenda doloribus porro velit
+                            fugit
+                            reprehenderit molestias voluptate a veritatis. Fugiat?
+                        </p>
+                    </div>
                 </div>
+            </div>
 
-                <h1 v-if="index != 2">
-                    {{ infoIngredients.text1 }}
+            <div class="card three">
+                <h5>
+                    Dont just take our word for it
+                </h5>
+
+                <h1>
+                    &ldquo;Finally found an alternative to the mass produced products. Something that incorporates real
+                    organic
+                    ingredients, nutrient dense wellness while promoting sustainability and activity.&rdquo;
                 </h1>
 
-                <h5 v-else>
-                    {{ infoIngredients.text1 }}
+                <h5>
+                    Rachel Cooper, Founder
                 </h5>
+            </div>
 
-                <h5 v-if="index != 2">
-                    {{ infoIngredients.text2 }}
-                </h5>
+            <div class="card four">
 
-                <p v-else>
-                    {{ infoIngredients.text2 }}
-                </p>
+                <div class="brightness">
+                    <div class="number">
+                        03
+                    </div>
 
-                <p v-if="index != 2">
-                    {{ infoIngredients.text3 }}
-                </p>
+                    <h1>
+                        Delivered to your home
+                    </h1>
 
-                <h5 v-else>
-                    {{ infoIngredients.text3 }}
-                </h5>
+                    <div class="text-card">
+                        <h5>
+                            Baked to order & Gift Wrapped
+                        </h5>
+
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi incidunt iure sed, facilis,
+                            ipsum
+                            necessitatibus blanditiis, quibusdam architecto aperiam assumenda doloribus porro velit
+                            fugit
+                            reprehenderit molestias voluptate a veritatis. Fugiat?
+                        </p>
+                    </div>
+                </div>
             </div>
 
         </div>
@@ -88,36 +124,84 @@ export default {
     flex-wrap: wrap;
 
     .card {
-        color: #000;
-        min-height: 400px;
         background-size: cover;
         width: 100%;
+        position: relative;
+
+        &.one {
+            .text-card {
+                padding-top: 80px;
+                float: right;
+                width: 420px;
+
+            }
+        }
+
+        &.three {
+            padding: 60px;
+
+            h1 {
+                color: $dark-violet-color;
+            }
+
+            h5 {
+                color: $text-color;
+                padding: 20px 0;
+            }
+        }
+
+        .brightness {
+            background-color: rgba(0, 0, 0, 0.4);
+            height: 100%;
+            padding: 60px;
+        }
+
+        &:not(.three) {
+            h1 {
+                color: $white-color;
+                width: 500px;
+            }
+
+            h5 {
+                color: rgba(255, 255, 255, 0.6);
+            }
+
+            p {
+                color: $white-color;
+                font-size: 0.8rem;
+                line-height: 23px;
+                padding: 15px 0;
+            }
+        }
+
+        &.two,
+        &.three {
+            width: 50%;
+        }
+
+        &.one {
+            background-image: url('/img/bakery-process-1.jpg');
+        }
+
+        &.two {
+            background-image: url('/img/bakery-process-2.jpg');
+        }
+
+        &.three {
+            background-color: #f2eef4;
+        }
+
+        &.four {
+            background-image: url('/img/bakery-process-3.jpg');
+        }
     }
 
-    .card.n-1,
-    .card.n-2 {
-        width: 50%;
-    }
 
-    .card.n-2 .number {
-        display: none;
-    }
 
-    .card.n-0 {
-        background-image: url('/img/bakery-process-1.jpg');
-        filter: brightness(70%);
-    }
 
-    .card.n-1 {
-        background-image: url('/img/bakery-process-2.jpg');
-    }
 
-    .card.n-2 {
-        background-color: #f2eef4;
-    }
 
-    .card.n-3 {
-        background-image: url('/img/bakery-process-3.jpg');
-    }
+
+
 }
 </style>
